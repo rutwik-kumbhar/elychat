6fb654ce232e2bda1a1adc7375d735b3432ab4ad
Rutwik Kumbhar <kumbharrutwik@gmail.com>
2025-03-21 00:53:53 +0530

    ely chat  service  created

---
+++ elychat-service/elychat-service/src/main/java/com/monocept/elychat/ElychatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.monocept.elychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElychatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElychatServiceApplication.class, args);
+	}
+
+}
---
+++ elychat-service/elychat-service/src/test/java/com/monocept/elychat/ElychatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monocept.elychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElychatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
